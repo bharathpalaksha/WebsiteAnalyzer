40cc4bc2d294d257f077ef8e6b52cf63a3e5b041
Bharath Palaksha <bharath.palaksha@hp.com>
2016-03-19 13:47:20 +0530

    MaxSum

--- MaxSum.java
+++ MaxSum.java
@@ -4,10 +4,16 @@ public class MaxSum {
 	
 	public static void main(String[] args) {
 		
-		int[] numbersList = {4,15,2};
+
+		int[] numbersList = {1,2};
 		
-		int big = numbersList[0], sum=0;
+		int big, sum=0;
 		
+		if(numbersList[0] > numbersList[1]) {
+			big = numbersList[0];
+		} else {
+			big = numbersList[1];
+		}
 		
 		for(int k=2;k<numbersList.length;k++) {
 		    for(int j=0;j<numbersList.length;j++) {
